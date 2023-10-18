052d69579590abee8340a309912932ed7af9d1f4
gablon29 <gabriellondero40@gmail.com>
2023-10-18 18:42:21 -0300

    first commit

---
+++ src/main/java/com/example/androidmaster/Variables.kt
@@ -0,0 +1,16 @@
+package com.example.androidmaster
+
+import kotlin.reflect.typeOf
+
+// Variables
+fun main() {
+    // Variables Numericas
+    // INT
+    val age:Int = 30
+    // LONG
+    val ageBig:Long = 3888499844849
+    //FLOAT
+    val ageRedondeado:Float = 3.54F
+    //
+}
+
