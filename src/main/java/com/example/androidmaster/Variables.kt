fa81d42f83ba0691472fe18db21489fcf0400141
gablon29 <gabriellondero40@gmail.com>
2023-10-18 18:50:33 -0300

    second

--- src/main/java/com/example/androidmaster/Variables.kt
+++ src/main/java/com/example/androidmaster/Variables.kt
@@ -5,12 +5,16 @@ import kotlin.reflect.typeOf
 // Variables
 fun main() {
     // Variables Numericas
+    // demo
     // INT
     val age:Int = 30
     // LONG
     val ageBig:Long = 3888499844849
     //FLOAT
     val ageRedondeado:Float = 3.54F
-    //
+    //Double => soporta 14 decimales
+    val doubleExample:Double = 3242.1515513
+
+
 }
 
